6fb6226d2c99ba1597ca21c9e12315d9a2701970
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-12-05 07:36:28 -0700

    s

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Galaga spinoff Project
+using System;
+using Raylib_cs;
+using System.Numerics;
+
+// Our code will include eight classes; player, Score, rock, gem, shooter, falling objects, Point, & Game. 
+
+namespace cse210_student_csharp_Greed
+{
+    // Main Function is to run the window and run the subsquent Class/Methods.
+    public class GreedGame
+    {
+        public static void Main()
+        {
+        while (!Raylib.WindowShouldClose())
+        {
+            
+        }
+        Raylib.CloseWindow();
+        }
+    }
+}
